a8b385edb5519d9af86a29e68168db4647c38492
natom24 <natom24@gmail.com>
2021-11-07 21:50:34 -0600

    First Push of code and population data retrieved from census.gov.

---
+++ Final Project/Code/Population_Data.R
@@ -0,0 +1,4 @@
+
+## Imports Data from The New York Times, based on reports from state and local health agencies.
+Countydata = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv")
+

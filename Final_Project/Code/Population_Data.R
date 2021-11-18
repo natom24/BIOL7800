1c95890653412c7a816bf26f4ab7abfaab52dc50
natom24 <natom24@gmail.com>
2021-11-18 13:02:08 -0600

    Added filteration of data

--- Final_Project/Code/Population_Data.R
+++ Final_Project/Code/Population_Data.R
@@ -1,5 +1,14 @@
+library("stringr")
+library("tidyverse")
 
 ## Imports Data from The New York Times, based on reports from state and local health agencies.
 Covid_data = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv")
+  Covid_data = Covid_data[!is.na(Covid_data$fips),] # Removes any fips values that are NA
 
+## Imports Data county population data from the census bureau. 
 County_data = read.csv("Final_Project/Downloaded_Data/co-est2020.csv")
+  County_data = County_data[!County_data$COUNTY == 0,] # Removes any county values labeled as 0 (total state population)
+  County_data$COUNTY = str_pad(County_data$COUNTY, 3 ,side = "left","0") # Adds 0s 
+  County_data$fips = str_c(County_data$STATE, "", County_data$COUNTY) # Crates fips labels to compare to covid dataset
+  County_data = County_data[c(22,6,7,21)] # Rearranges data into more readable format
+  

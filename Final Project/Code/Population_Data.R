ea1dbf936307020c9823df0a906f71eab5494da9
natom24 <natom24@gmail.com>
2021-11-17 22:32:33 -0600

    Renamed Files

--- Final Project/Code/Population_Data.R
+++
@@ -1,4 +0,0 @@
-
-## Imports Data from The New York Times, based on reports from state and local health agencies.
-Countydata = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv")
-
---
+++ Final_Project/Code/Population_Data.R
@@ -0,0 +1,5 @@
+
+## Imports Data from The New York Times, based on reports from state and local health agencies.
+Covid_data = read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv")
+
+County_data = read.csv("Final_Project/Downloaded_Data/co-est2020.csv")
